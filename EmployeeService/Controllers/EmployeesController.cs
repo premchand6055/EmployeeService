93a9c15a9eadf8e6c546b705bfcc79a79746c461
premchand <premchand6055@gmail.com>
2024-09-22 15:09:45 +0530

    Add project files.

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeemanagementDbContext _context;
+        public EmployeesController(EmployeemanagementDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+    }
+}
